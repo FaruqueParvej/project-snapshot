2bd20d3c479fca7558953568baeaf8b33c615241
FaruqueParvej <omarfaruque1750@gmail.com>
2024-05-14 15:48:55 +0600

    edited logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import LogoSvg from "./logo.svg";
 export default function Logo() {
   return (
     <div>
       <Link href="/">
         <Image
           className="max-w-[100px] md:max-w-[165px] "
-          src={LogoSvg}
+          src="/logo.svg"
           alt="Lws"
           height={50}
           width={100}
